ded5432e4a95152c6cb7f952330bbf81245a0d8c
yuunull <yuunull2525@gmail.com>
2022-11-13 23:05:07 +0900

    first commit

---
+++ abc276/Rightmost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputValue = Console.ReadLine();
+            var result = -1;
+            for (var i = 0; i < inputValue.Length; i++)
+            {
+                if (inputValue.Substring(i,1).Equals("a")) {
+                    result = i + 1;
+                };
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
