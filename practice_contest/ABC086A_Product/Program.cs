c2cc3abcbc8ab73bb0844fad40545a9db6575182
yuunull <yuunull2525@gmail.com>
2022-11-30 00:03:10 +0900

    add

---
+++ practice_contest/ABC086A_Product/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            var a = Convert.ToInt32(input[0]);
+            var b = Convert.ToInt32(input[1]);
+
+            if(IsEven(a, b))
+            {
+                Console.WriteLine("Even");
+            } 
+            else 
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+
+        private static bool IsEven(int a, int b) {
+            return a * b % 2 == 0;
+        }
+    }
+}
