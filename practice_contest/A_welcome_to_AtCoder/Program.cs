cfc7b25230844bba3e73a150be0e06ba5d8a82db
yuunull <yuunull2525@gmail.com>
2022-11-29 23:46:54 +0900

    add practice

---
+++ practice_contest/A_welcome_to_AtCoder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line1 = Console.ReadLine();
+            string line2 = Console.ReadLine();
+            string line3 = Console.ReadLine();
+
+            var a = Convert.ToInt32(line1);
+            var bArray = line2.Split(" ");
+            var b1 = Convert.ToInt32(bArray[0]);
+            var b2 = Convert.ToInt32(bArray[1]);
+
+
+            Console.WriteLine($"{a + b1 + b2} {line3}");
+        }
+    }
+}
