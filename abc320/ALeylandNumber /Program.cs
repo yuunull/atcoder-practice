2d5b0d9de7888ba0b69e86631b38caaef93cffef
yuunull <yuunull2525@gmail.com>
2023-09-22 23:27:50 +0900

    abc320

---
+++ abc320/ALeylandNumber /Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sArray = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(sArray[0]);
+            int b = int.Parse(sArray[1]);
+
+            var left = Math.Pow(a, b);
+            var right = Math.Pow(b, a);
+
+            Console.WriteLine(left + right);
+        }
+    }
+}
