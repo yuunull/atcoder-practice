957ab50ce9b38eb6492ff1f4475674f073386d57
yuunull <yuunull2525@gmail.com>
2023-10-07 23:11:49 +0900

    []

---
+++ kyopro_tessoku/AnswerA07.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class AnswerA07
+{
+    public void Run()
+    {
+        Console.WriteLine("aaa成功！！");
+    }
+}
---
+++ kyopro_tessoku/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("クラス名を指定してください。");
+            return;
+        }
+
+        string className = args[0];
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        Type type = assembly.GetType(className);
+
+        if (type == null)
+        {
+            Console.WriteLine($"指定されたクラス '{className}' が見つかりません。");
+            return;
+        }
+
+        // クラスのインスタンスを作成
+        object instance = Activator.CreateInstance(type);
+
+        // クラスのメソッドを実行
+        MethodInfo method = type.GetMethod("Run");
+        if (method != null)
+        {
+            method.Invoke(instance, null);
+        }
+        else
+        {
+            Console.WriteLine($"クラス '{className}' に 'Run' メソッドが見つかりません。");
+        }
+    }
+}
