c862e93805a1c6181315768e15ab1d37cf8da26f
yuunull <yuunull2525@gmail.com>
2023-10-07 23:11:07 +0900

    [add]abc323 a

---
+++ abc323/WeakBeats/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] charArray = input.ToCharArray();
+
+            bool isYes = true;
+            for (int i = 1; i < charArray.Length + 1; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (int.Parse(charArray[i - 1].ToString()) != 0)
+                    {
+                        isYes = false;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(isYes ? "Yes" : "No");
+        }
+    }
+}
