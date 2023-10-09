9324337f231de378ce2c76dfd040b0fe84d2e43d
yuunull <yuunull2525@gmail.com>
2023-10-09 22:43:28 +0900

    [add]abc318 a

---
+++ abc318/FullMoon/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nmp = Console.ReadLine().Split(' ');
+            int n = int.Parse(nmp[0]);
+            int m = int.Parse(nmp[1]);
+            int p = int.Parse(nmp[2]);
+
+            int cnt = 0;
+            if(n >= m){
+                bool isNoFullMoon = true;
+                int currentDay = m;
+                while(isNoFullMoon) {
+                    currentDay += p;
+                    if(currentDay > n) {
+                        isNoFullMoon = false;
+                    }
+                    cnt++;
+                }
+            }
+            else
+            {
+                cnt = 0;
+            }
+
+            Console.WriteLine(cnt);
+        }
+    }
+}
