c43124fe925b81f1b95a8e15d7ebdd0aa3c6cbda
yuunull <yuunull2525@gmail.com>
2023-10-09 22:06:59 +0900

    [add]abc323 C

---
+++ abc323/MergeSlimes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] orders = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                string[] sc = Console.ReadLine().Split(' ');
+                orders[i] = new int[] { int.Parse(sc[0]), int.Parse(sc[1])};
+            }
+
+            // ABの問題が15分以内で解けるようになったらここにかえってくる
+        }
+    }
+}
