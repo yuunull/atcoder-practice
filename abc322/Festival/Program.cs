94b958119efd68f1412fc8e33417ed77f36cb280
yuunull <yuunull2525@gmail.com>
2023-10-10 23:39:06 +0900

    [add]abc322 c

---
+++ abc322/Festival/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nm = Console.ReadLine().Split(' ');
+            int n = int.Parse(nm[0]);
+            int m = int.Parse(nm[1]);
+            string[] sArray = Console.ReadLine().Split(' ');
+
+            bool[] everyDays = new bool[n];
+            for (int i = 0; i < n; i++) {
+                everyDays[i] = false;
+            }
+
+            for (int j = 0; j < m; j++) {
+                everyDays[int.Parse(sArray[j]) - 1] = true;
+            }
+
+            int cnt = 0;
+            int[] resultArray = new int[n];
+            for (int k = n - 1; k >= 0; k--) {
+                if (everyDays[k]) {
+                    resultArray[k] = 0;
+                    cnt = 0;
+                }
+                else
+                {
+                    cnt++;
+                    resultArray[k] = cnt;
+                }
+            }
+
+            foreach (int result in resultArray) {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
