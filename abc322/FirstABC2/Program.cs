05370b322c66ab494163c7016715787cf3df0eed
yuunull <yuunull2525@gmail.com>
2023-10-10 22:27:18 +0900

    [add]abc322 a

---
+++ abc322/FirstABC2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[] charArray = Console.ReadLine().ToCharArray();
+
+            int abcIndex = -1;
+            for(int i = 0; i < n - 2; i++) {
+                if(charArray[i].ToString() == "A") {
+                    if(charArray[i + 1].ToString() == "B") {
+                        if(charArray[i + 2].ToString() == "C") {
+                            abcIndex = i + 1;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(abcIndex);
+        }
+    }
+}
