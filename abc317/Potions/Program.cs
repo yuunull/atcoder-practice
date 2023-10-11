0aeeb9d14152f5c5e7a1812ce1e79bdccf9fc47d
yuunull <yuunull2525@gmail.com>
2023-10-11 23:29:59 +0900

    [add]abc317 a

---
+++ abc317/Potions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nhx = Console.ReadLine().Split(' ');
+            int n = int.Parse(nhx[0]);
+            int h = int.Parse(nhx[1]);
+            int x = int.Parse(nhx[2]);
+            string[] pArray = Console.ReadLine().Split(' ');
+
+            int a = x - h;
+            int result = 0;
+            for (int i = 0; i < n; i++) {
+                if (a <= int.Parse(pArray[i])) {
+                    result = i + 1;
+                    break;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
