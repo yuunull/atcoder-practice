f3ea9d12604227765424cbfc1e3c4707c9a38f62
yuunull <yuunull2525@gmail.com>
2023-10-11 23:48:53 +0900

    [add]abc317 c

---
+++ abc317/RememberingTheDays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            Array.Sort(array);
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] - array[i - 1] == 2)
+                {
+                    Console.WriteLine(array[i] - 1);
+                    break;
+                }
+            }
+        }
+    }
+}
