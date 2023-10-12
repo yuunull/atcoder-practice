42a6dff7e8a46bdf638697f4b0b1f04f6465ffb5
yuunull <yuunull2525@gmail.com>
2023-10-13 00:24:12 +0900

    [add]abc315 a

---
+++ abc315/tcdr/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] sArray = Console.ReadLine().ToCharArray();
+
+            string result = string.Empty;
+            for (int i = 0; i < sArray.Length; i++) 
+            {
+                if(sArray[i].ToString() == "a") {
+                    continue;
+                }
+
+                if(sArray[i].ToString() == "i") {
+                    continue;
+                }
+
+                if(sArray[i].ToString() == "u") {
+                    continue;
+                }
+
+                if(sArray[i].ToString() == "e") {
+                    continue;
+                }
+
+                if(sArray[i].ToString() == "o") {
+                    continue;
+                }
+                result += sArray[i].ToString();
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
