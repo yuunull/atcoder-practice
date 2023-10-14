6c52e97614c31ccffae1a4d77c73748b00ec0bc4
yuunull <yuunull2525@gmail.com>
2023-10-14 23:41:11 +0900

    [add]abc324 b

---
+++ abc324/3SmoothNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            
+            bool isYes = false;
+            for (int x = 0; x < 100; x++)
+            {
+                double twoX = Math.Pow(2, x);
+                for (int y = 0; y < 100; y++)
+                {
+                    double threeY = Math.Pow(3, y);
+                    if (twoX * threeY == n) {
+                        isYes = true;
+                        break;
+                    }
+                }
+
+                if (isYes) 
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(isYes ? "Yes": "No");
+        }
+    }
+}
