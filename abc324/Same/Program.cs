ce8648b46b5a7ab2e581d123ff9406d7125646cf
yuunull <yuunull2525@gmail.com>
2023-10-14 23:40:41 +0900

    [add]abc324 a

---
+++ abc324/Same/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            bool isSame = true;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[0] != array[i])
+                {
+                    isSame = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine(isSame ? "Yes": "No");
+        }
+    }
+}
