d2d8670bcb42acda2d6ce8192dc9373d7c5027f2
yuunull <yuunull2525@gmail.com>
2023-10-16 23:48:22 +0900

    [add]code fes a

---
+++ CODE_FESTIVAL_2016qualC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] s = Console.ReadLine().ToCharArray();
+            bool isCF = false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i].ToString() == "C")
+                {
+                    for (int j = i + 1; j < s.Length; j++)
+                    {
+                        if (s[j].ToString() == "F")
+                        {
+                            isCF = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(isCF ? "Yes" : "No");
+        }
+    }
+}
