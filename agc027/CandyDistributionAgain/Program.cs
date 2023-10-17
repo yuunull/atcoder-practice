52786c68cc70dedb293946be5ab9b5dfe84ea7bf
yuunull <yuunull2525@gmail.com>
2023-10-17 22:07:51 +0900

    [add]agc027 a

---
+++ agc027/CandyDistributionAgain/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nx = Console.ReadLine().Split();
+            int n = int.Parse(nx[0]);
+            int x = int.Parse(nx[1]);
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            Array.Sort(array);
+
+            int cnt = 0;
+            for (int j = 0; j < array.Length; j++)
+            {
+                x -= array[j];
+                if (x >= 0)
+                {
+                    cnt++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(cnt);
+        }
+    }
+}
