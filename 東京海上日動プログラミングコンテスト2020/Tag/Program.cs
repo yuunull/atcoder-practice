2fb8bc8f064f585e0faa55413a94ca2262b016f9
yuunull <yuunull2525@gmail.com>
2023-10-17 22:48:37 +0900

    [add]

---
+++ 東京海上日動プログラミングコンテスト2020/Tag/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] av = Console.ReadLine().Split();
+            string[] bw = Console.ReadLine().Split();
+            int t = int.Parse(Console.ReadLine());
+            int a = int.Parse(av[0]);
+            int v = int.Parse(av[1]);
+            int b = int.Parse(bw[0]);
+            int w = int.Parse(bw[1]);
+
+            bool isCatch = false;
+            for (int i = 0; i < t; i++)
+            {
+                a += v;
+                b += w;
+                if (a >= b)
+                {
+                    isCatch = true;
+                    break;
+                }
+            }
+
+            Console.WriteLine(isCatch ? "YES" : "NO");
+        }
+    }
+}
