a32ed1af1b621cebe379503e04f1e8f70eb22d81
yuunull <yuunull2525@gmail.com>
2023-10-17 23:39:49 +0900

    [add]abc071 b

---
+++ abc071/NotFound/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] s = Console.ReadLine().ToCharArray();
+            char[] sortedDistinctChars = s.Distinct().OrderBy(c => c).ToArray();
+            string all = "abcdefghijklmnopqrstuvwxyz";
+
+            if(new string(sortedDistinctChars) == all)
+            {
+                Console.WriteLine("None");
+            }
+            else
+            {
+                string result = "";
+                int index = 0;
+                char[] allArray = all.ToCharArray();
+                foreach(char val in allArray)
+                {
+                    if (index >= sortedDistinctChars.Length || val != sortedDistinctChars[index])
+                    {
+                        result = val.ToString();
+                        break;
+                    }
+                    index++;
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
