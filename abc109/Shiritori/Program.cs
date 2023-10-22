8bc95eff3899fdd6fb87f437cae75fdc0a9d595a
yuunull <yuunull2525@gmail.com>
2023-10-22 11:34:36 +0900

    [add]abc109 b

---
+++ abc109/Shiritori/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string[] array = new string[n];
+        for(int i = 0; i < n; i++)
+        {
+            array[i] = Console.ReadLine();
+        }
+
+        bool isYes = true;
+        if(array.Length != array.Distinct().Count())
+        {
+            isYes = false;
+        }
+
+        if(isYes)
+        {
+            for(int i = 0; i < n - 1; i++)
+            {
+                if(array[i][array[i].Length - 1] != array[i + 1][0])
+                {
+                    isYes = false;
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine(isYes ? "Yes" : "No");
+    }
+}
