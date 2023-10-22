696178c4f6f321443a5e7db5b2527648600b3a7f
yuunull <yuunull2525@gmail.com>
2023-10-22 10:44:12 +0900

    [add]abc035 a

---
+++ abc035/高橋くんと回文/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        char[] sArray = Console.ReadLine().ToCharArray();
+        char[] reversed = sArray.Reverse().ToArray();
+
+        bool isYes = true;
+        for(int i = 0; i < sArray.Length; i++) 
+        {
+            if(sArray[i].ToString() == "*" || reversed[i].ToString() == "*") continue;
+            if(sArray[i] != reversed[i])
+            {
+                isYes = false;
+                break;
+            }
+        }
+
+        Console.WriteLine(isYes ? "YES" : "NO");
+    }
+}
