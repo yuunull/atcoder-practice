4fe0049ce2a7d6233ab47589f67328a51c19295c
yuunull <yuunull2525@gmail.com>
2023-10-22 15:21:33 +0900

    [add]

---
+++ abc226/CountingArrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string[] array = new string[n];
+        for(int i = 0; i < n; i++)
+        {
+            array[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine(array.Distinct().Count());
+    }
+}
---
+++ abc291/LRUDInstructions2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        char[] array = Console.ReadLine().ToCharArray();
+
+        string[] moveArray = new string[n + 1];
+        int x = 0;
+        int y = 0;
+        moveArray[0] = "00";
+        for(int i = 1; i < n + 1; i++)
+        {
+            switch(array[i - 1].ToString())
+            {
+                case "R":
+                    x += 1;
+                    break;
+                case "L":
+                    x -= 1;
+                    break;
+                case "U":
+                    y += 1;
+                    break;
+                case "D":
+                    y -= 1;
+                    break;
+            }
+            moveArray[i] = x.ToString() + y.ToString();
+        }
+
+        bool isYes = false;
+        if(moveArray.Length - moveArray.Distinct().Count() > 0)
+        {
+            isYes = true;
+        }
+
+        Console.WriteLine(isYes ? "Yes" : "No");
+    }
+}
