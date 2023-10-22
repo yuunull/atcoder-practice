b9ecd4eb4fa1cdf5f3b9c9055330c129acc12f8f
yuunull <yuunull2525@gmail.com>
2023-10-22 11:19:18 +0900

    [add]arc015 a

---
+++ arc015/CelsiusとFahrenheit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(((double)9 / 5 * n) + 32);
+    }
+}
