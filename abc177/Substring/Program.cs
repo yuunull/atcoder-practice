14c92b644999462c470ea53717b68154f107971c
yuunull <yuunull2525@gmail.com>
2023-10-22 16:44:19 +0900

    [add]abc177 b

---
+++ abc177/Substring/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        string t = Console.ReadLine();
+
+        int result = t.Length;
+        for(int i = 0; i <= s.Length - t.Length; i++)
+        {
+            int diff = 0;
+            for(int j = 0; j < t.Length; j++)
+            {
+                if(t[j] != s[i + j])
+                {
+                    diff++;
+                }
+            }
+
+            if(result > diff){
+                result = diff;
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
