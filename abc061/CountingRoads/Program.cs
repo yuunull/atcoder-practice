2c3acd421cbad4fc1a6ae273e5f9c2dacd4bb81f
yuunull <yuunull2525@gmail.com>
2023-10-22 23:13:44 +0900

    [add]abc061 b

---
+++ abc061/CountingRoads/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] nm = Console.ReadLine().Split();
+        int n = int.Parse(nm[0]);
+        int m = int.Parse(nm[1]);
+
+        int[] result = new int[n];
+        for(int i = 0; i < m; i++)
+        {
+            string[] ab = Console.ReadLine().Split();
+            int a = int.Parse(ab[0]);
+            int b = int.Parse(ab[1]);
+            result[a - 1] += 1;
+            result[b - 1] += 1;
+        }
+
+        for(int i = 0; i < result.Length; i++){
+            Console.WriteLine(result[i]);
+        }
+    }
+}
