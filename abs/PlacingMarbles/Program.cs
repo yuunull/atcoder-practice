6f05f2cebb8e3d39ef47548e6b89f4ca2bbbab64
yuunull <yuunull2525@gmail.com>
2023-10-25 00:14:09 +0900

    add

---
+++ abs/PlacingMarbles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        char[] sArray = Console.ReadLine().ToCharArray();
+        Console.WriteLine(sArray.Count(r => r.ToString() == "1"));
+    }
+}
