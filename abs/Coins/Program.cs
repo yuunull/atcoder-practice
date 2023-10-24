732cc68800ddef4c3ca0b0e2b9612d512f3a9025
yuunull <yuunull2525@gmail.com>
2023-10-24 23:59:45 +0900

    add

---
+++ abs/Coins/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+        int x = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(CountWaysToMakeAmount(a, b, c, x));
+    }
+
+    static int CountWaysToMakeAmount(int a, int b, int c, int x)
+    {
+        if (x == 0) return 1;
+
+        if (x < 0 || (a == 0 && b == 0 && c == 0)) return 0;
+
+        int ways = 0;
+
+        if (a > 0) ways += CountWaysToMakeAmount(a - 1, b, c, x - 500);
+        if (b > 0) ways += CountWaysToMakeAmount(a, b - 1, c, x - 100);
+        if (c > 0) ways += CountWaysToMakeAmount(a, b, c - 1, x - 50);
+
+        return ways;
+    }
+}
