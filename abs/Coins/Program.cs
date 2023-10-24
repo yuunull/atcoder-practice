d538ce28b2eba01fa75a9c415d60a38ff3666a61
yuunull <yuunull2525@gmail.com>
2023-10-25 00:18:27 +0900

    add

--- abs/Coins/Program.cs
+++ abs/Coins/Program.cs
@@ -11,48 +11,22 @@ class Program
         int c = int.Parse(Console.ReadLine());
         int x = int.Parse(Console.ReadLine());
 
-        int[] choices = new int[a + b + c];
-        for(int i = 0; i < a; i++)
-        {
-            choices[i] = 500;
-        }
-
-        for(int i = a; i < a + b; i++)
-        {
-            choices[i] = 100;
-        }
-
-        for(int i = a + b; i < a + b + c; i++)
-        {
-            choices[i] = 50;
-        }
-
         int count = 0;
-        List<int> currentCombination = new List<int>();
-        DFS(0, x, choices, currentCombination, count);
+
+        for(var i = 0; i <= a; i++)
+        {
+            for(var j = 0; j <= b; j++)
+            {
+                for( var k = 0; k <= c; k++)
+                {
+                    if ((500 * i + 100 * j + 50 * k) == x)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
         
         Console.WriteLine(count);
-    }
-
-    static void DFS(int currentNumber, int targetNumber, int[] choices, List<int> currentCombination, int count)
-    {
-        if (currentNumber == targetNumber)
-        {
-            count++;
-            return;
-        }
-
-        if (choices.Length == 0 || currentNumber > targetNumber)
-        {
-            return;
-        }
-
-        for (int i = 0; i < choices.Length; i++)
-        {
-            int choice = choices[i];
-            currentCombination.Add(choice);
-            DFS(currentNumber + choice, targetNumber, choices[i..], currentCombination, count);
-            currentCombination.RemoveAt(currentCombination.Count - 1);
-        }
     }
 }
