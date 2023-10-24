16647b5fc28ac422b8d4a66a5b9e92ccabcbf59c
yuunull <yuunull2525@gmail.com>
2023-10-24 22:36:25 +0900

    [add]abc251 b

---
+++ abc251/AtMost3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] nw = Console.ReadLine().Split();
+        int n = int.Parse(nw[0]);
+        int w = int.Parse(nw[1]);
+        int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        Array.Sort(numbers);
+
+        // 途中でやめた
+    }
+}
