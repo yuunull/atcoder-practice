1765b5345e73485324998e462b14aeb5ccf53f24
yuunull <yuunull2525@gmail.com>
2023-10-25 00:14:17 +0900

    add

---
+++ abs/Product/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int[] ab = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        Console.WriteLine(ab[0] * ab[1] % 2 == 0 ? "Even" : "Odd");
+    }
+}
