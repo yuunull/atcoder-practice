a49f2aa964ca13d64b6f8a362bb4ffe0f79da858
yuunull <yuunull2525@gmail.com>
2023-10-25 00:14:26 +0900

    add

---
+++ abs/ShiftOnly/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        long[] aArray = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+
+        int count = 0;
+        while (aArray.All(r => r % 2 == 0))
+        {
+            aArray = aArray.Select(r => r / 2).ToArray();
+            count++;
+
+            if (aArray.Any(r => r % 2 != 0))
+            {
+                break;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
