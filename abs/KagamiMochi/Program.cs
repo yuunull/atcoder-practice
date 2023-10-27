3cb7592a220e557595b695750a29da078e9eb760
yuunull <yuunull2525@gmail.com>
2023-10-27 21:31:23 +0900

    [add]abs

---
+++ abs/KagamiMochi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] array = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        int[] intArray = array.Distinct().ToArray();
+        Console.WriteLine(intArray.Count());
+    }
+}
