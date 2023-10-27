fa8cb1406a7e8db7c1b5bf17220f453a96a98151
yuunull <yuunull2525@gmail.com>
2023-10-27 21:24:18 +0900

    [add]abs

---
+++ abs/CardGameforTwo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string[] aArray = Console.ReadLine().Split();
+        int[] intArray = aArray.Select(s => int.Parse(s)).OrderByDescending(num => num).ToArray();
+
+        int alice = 0;
+        int bob = 0;
+        for(int i = 1; i < n + 1; i++)
+        {
+            if(i % 2 != 0)
+            {
+                alice += intArray[i - 1];
+            }
+            else
+            {
+                bob += intArray[i - 1];
+            }
+        }
+        
+        Console.WriteLine(alice - bob);
+    }
+}
