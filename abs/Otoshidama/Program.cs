3cc95b0e096f47200b2fbb9080e5a9a947473233
yuunull <yuunull2525@gmail.com>
2023-10-27 22:11:13 +0900

    [add]abs

---
+++ abs/Otoshidama/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] s = Console.ReadLine().Split(' ');
+        int n = int.Parse(s[0]);
+        int y = int.Parse(s[1]);
+
+        for(int i = 0; i <= n; i++)
+        {
+            if(10000 * i > y) continue;
+            for(int j = n - i; j >= 0; j--)
+            {
+                if(10000 * i + 5000 * j + 1000 * (n - i - j) == y)
+                {
+                    Console.WriteLine(i + " " + j + " " + (n - i - j));
+                    return;
+                }
+            }
+        }
+        Console.WriteLine("-1 -1 -1");
+    }
+}
