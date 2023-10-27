1f7d7febb1d002966cf14afba2138d4f01b41043
yuunull <yuunull2525@gmail.com>
2023-10-27 21:11:12 +0900

    [add]abs

---
+++ abs/SomeSums/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] nab = Console.ReadLine().Split();
+        int n = int.Parse(nab[0]);
+        int a = int.Parse(nab[1]);
+        int b = int.Parse(nab[2]);
+
+        List<int> someArray = new List<int>();
+        for(int i = 1; i <= n; i++)
+        {
+            int sum = i.ToString().ToCharArray().Select(c => (int)Char.GetNumericValue(c)).ToArray().Sum();
+            if(sum >= a && sum <= b)
+            {
+                someArray.Add(i);
+            }
+        }
+        
+        Console.WriteLine(someArray.Sum());
+    }
+}
