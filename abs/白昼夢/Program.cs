b80c0ce0f49fb6c3e1e1fd3821f0123af1a9c9ac
yuunull <yuunull2525@gmail.com>
2023-10-27 23:08:54 +0900

    [add]abs

---
+++ abs/白昼夢/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        string[] tEnds = { "eraser", "erase", "dreamer", "dream" };
+
+        string ans = "YES";
+        while (!string.IsNullOrEmpty(s))
+        {
+            var endswithAny = false;
+            for (int i = 0; i < tEnds.Length; i++)
+            {
+                if (s.EndsWith(tEnds[i]))
+                {
+                    // 後ろから切る
+                    endswithAny = true;
+                    s = s[..(s.Length - tEnds[i].Length)];
+                    break;
+                }
+            }
+            if (!endswithAny)
+            {
+                ans = "NO";
+                break;
+            }
+
+        }
+
+        Console.WriteLine(ans);
+    }
+}
