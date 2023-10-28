7f55a81efacf4638e6fdee5ab725be556378cc6c
yuunull <yuunull2525@gmail.com>
2023-10-28 12:01:35 +0900

    [update]

--- abc251/AtMost3/Program.cs
+++ abc251/AtMost3/Program.cs
@@ -12,6 +12,43 @@ class Program
         int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
         Array.Sort(numbers);
 
-        // 途中でやめた
+       List<int> result = new List<int>();
+       for(int i = 0; i < n; i++)
+       {
+            if(numbers[i] <= w)
+            {
+                result.Add(numbers[i]);
+            }
+            else
+            {
+                break;
+            }
+
+            for(int j = i + 1; j < n; j++)
+            {
+                if(numbers[i] + numbers[j] <= w)
+                {
+                    result.Add(numbers[i] + numbers[j]);
+                }
+                else
+                {
+                    break;
+                }
+
+                for(int k = j + 1; k < n; k++)
+                {
+                    if(numbers[i] + numbers[j] + numbers[k] <= w)
+                    {
+                        result.Add(numbers[i] + numbers[j] + numbers[k]);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+       }
+
+       Console.WriteLine(result.Distinct().Count());
     }
 }
