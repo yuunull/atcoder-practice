56349c47ea24604d78619a1db695dc367c8a89bc
yuunull <yuunull2525@gmail.com>
2023-10-28 23:05:36 +0900

    [add]abc326 a

---
+++ abc326/2UP3DOWN/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] xy = Console.ReadLine().Split();
+        int x = int.Parse(xy[0]);
+        int y = int.Parse(xy[1]);
+
+        if(x > y)
+        {
+            Console.WriteLine(x - y <= 3 ? "Yes" : "No");
+        }
+        else
+        {
+            Console.WriteLine(y - x <= 2 ? "Yes" : "No");
+        }
+    }
+}
