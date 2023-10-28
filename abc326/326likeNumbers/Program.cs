c7e61ba9d2a7693bad80a25895b93dae4c523b5f
yuunull <yuunull2525@gmail.com>
2023-10-28 23:05:49 +0900

    [add]abc326 b

---
+++ abc326/326likeNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        bool isOk = true;
+        while(isOk)
+        {
+            char[] array = n.ToString().ToCharArray();
+            int x = int.Parse(array[0].ToString());
+            int y = int.Parse(array[1].ToString());
+            int z = int.Parse(array[2].ToString());
+            if(x * y == z)
+            {
+                Console.WriteLine(n);
+                isOk = false;
+                break;
+            }
+            n++;
+        }
+    }
+}
