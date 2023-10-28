d7b35fe09a2ccf62e227ce6cc1fc419422010877
yuunull <yuunull2525@gmail.com>
2023-10-28 12:24:59 +0900

    [add]abc148 d

---
+++ abc148/BrickBreak/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+        bool isSatisfaction = false;
+        int target = 1;
+        int result = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(numbers[i] == target)
+            {
+                isSatisfaction = true;
+                target++;
+            }
+            else
+            {
+                result++;
+            }
+        }
+
+       Console.WriteLine(isSatisfaction ? result : -1);
+    }
+}
