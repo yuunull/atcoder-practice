7799c07b9fbae80be60e6f06f17e8f195068fbb7
yuunull <yuunull2525@gmail.com>
2023-10-28 13:07:53 +0900

    [add]abc231 c

--- abc231/Counting2/Program.cs
+++ abc231/Counting2/Program.cs
@@ -9,59 +9,29 @@ class Program
         string[] nq = Console.ReadLine().Split();
         int n = int.Parse(nq[0]);
         int q = int.Parse(nq[1]);
-        int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-        Array.Sort(numbers);
+        List<long> numbers = Console.ReadLine().Split().Select(long.Parse).OrderBy(r => r).ToList();
 
-        int[] result = new int[q];
+        long[] xArray = new long[q];
         for(int i = 0; i < q; i++)
         {
-            int x = int.Parse(Console.ReadLine());
-
-            int mid1 = 0;
-            int low = 0;
-            int high = numbers.Length - 1;
-
-            // xより小さい最大の数値のindexを取得する
-            while (low <= high)
-            {
-                mid1 = (low + high) / 2;
-
-                if (x >= numbers[mid1])
-                {
-                    low = mid1 + 1;
-                }
-                else
-                {
-                    high = mid1 - 1;
-                }
-            }
-
-            int mid2 = 0;
-            low = 0;
-            high = numbers.Length - 1;
-
-            // xより大きい最小の数値のindexを取得する
-            while (low <= high)
-            {
-                mid2 = (low + high) / 2;
-
-                if (x >= numbers[mid2])
-                {
-                    low = mid2 + 1;
-                }
-                else
-                {
-                    high = mid2 - 1;
-                }
-            }
-
-            Console.WriteLine(mid1.ToString() + mid2.ToString());
-            result[i] = mid1 - mid2;
+            xArray[i] = long.Parse(Console.ReadLine());
         }
+        
+        foreach(int i in xArray)
+        {
+            var index = numbers.BinarySearch(i);
+            if (index < 0)
+            {
+                index = ~index;
+            }
 
-        // for(int i = 0; i < q; i++)
-        // {
-        //     Console.WriteLine(result[i]);
-        // }
+            int result = n - index;
+            if (result < 0)
+            {
+                result = 0;
+            }
+
+            Console.WriteLine(result);
+        }
     }
 }
