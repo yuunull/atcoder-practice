419f26a75fe3e63c48d27d323f2eadfc1ff60337
yuunull <yuunull2525@gmail.com>
2023-10-29 10:20:03 +0900

    [add]apc001 a

---
+++ apc001/TwoIntegers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] xy = Console.ReadLine().Split();
+        int x = int.Parse(xy[0]);
+        int y = int.Parse(xy[1]);
+
+        if (x % y == 0)
+        {
+            Console.WriteLine(-1);
+            return;
+        }
+
+        Console.WriteLine(x);
+    }
+}
