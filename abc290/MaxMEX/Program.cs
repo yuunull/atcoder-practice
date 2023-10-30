1665b4b7d32f4ed05609c4b619c284eb5f8eb87b
yuunull <yuunull2525@gmail.com>
2023-10-30 21:39:57 +0900

    [add]abc290 c

---
+++ abc290/MaxMEX/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] nk = Console.ReadLine().Split();
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = int.Parse(nk[0]);
+        int k = int.Parse(nk[1]);
+
+        HashSet<int> hash = new HashSet<int>(a);
+        for(int i = 0; i < k; i++)
+        {
+            if(!hash.Contains(i))
+            {
+                Console.WriteLine(i);
+                return;
+            }
+        }
+        Console.WriteLine(k);
+    }
+}
