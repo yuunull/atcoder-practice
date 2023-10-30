30a56a9868341daba1532b2098288e0a6ff2d603
yuunull <yuunull2525@gmail.com>
2023-10-30 21:25:46 +0900

    [add]abc290 b

---
+++ abc290/QualB/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] nk = Console.ReadLine().Split();
+        char[] s = Console.ReadLine().ToCharArray();
+        int n = int.Parse(nk[0]);
+        int k = int.Parse(nk[1]);
+
+        string result = string.Empty;
+        int count = 0;
+        for(int i = 0; i < n; i++)
+        {
+            result += s[i].ToString();
+            if(s[i].ToString() == "o")
+            {
+                count++;
+                if(count == k)
+                {
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine(result.PadRight(n, 'x'));
+    }
+}
