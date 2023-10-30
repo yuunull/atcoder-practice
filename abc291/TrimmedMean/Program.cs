15d27e9985e97dbbed1102825cc5047ce2668c62
yuunull <yuunull2525@gmail.com>
2023-10-30 21:54:51 +0900

    [add]abc291 b

---
+++ abc291/TrimmedMean/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] x = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Array.Sort(x);
+
+        int[] newArray = x.Skip(n).Take(x.Length - 2 * n).ToArray();
+        
+        decimal sum = newArray.Sum();
+        decimal newN = newArray.Count();
+        Console.WriteLine(sum / newN);
+    }
+}
