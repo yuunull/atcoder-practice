97b9bc00f9f563640ee2cbf4ffaff4ecd577a803
yuunull <yuunull2525@gmail.com>
2023-10-30 22:03:25 +0900

    [add]abc292 a

---
+++ abc292/CAPSLOCK/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(s.ToUpper());
+    }
+}
