20e0c78469ee98e00bbbeb0fadddf0334b0b901e
yuunull <yuunull2525@gmail.com>
2023-10-30 23:10:50 +0900

    [add]abc292 c

---
+++ abc292/FourVariables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        long[] counts = new long[n];
+
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                int v = i * j;
+                if (v >= n)
+                {
+                    break;
+                }
+                counts[v]++;
+            }
+        }
+
+        long sum = 0;
+        for (int i = 1; i < n; i++)
+        {
+            sum += counts[i] * counts[n - i];
+        }
+
+        Console.WriteLine(sum);
+    }
+}
