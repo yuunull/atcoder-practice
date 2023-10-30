fbb86baf49fbc60bb670a6fb60397bf988880c95
yuunull <yuunull2525@gmail.com>
2023-10-30 21:46:44 +0900

    [add]abc291 a

---
+++ abc291/camelCase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string s = Console.ReadLine();
+        
+        int index = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (Char.IsUpper(s[i]))
+            {
+                Console.WriteLine(i + 1);
+                return;
+            }
+        }
+    }
+}
