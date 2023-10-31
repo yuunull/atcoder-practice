10ae443d6b7c20ee1159d9e8189af6f9ea7baff8
yuunull <yuunull2525@gmail.com>
2023-10-31 21:28:30 +0900

    [add]abc293 a

---
+++ abc293/SwapOddAndEven/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        char[] s = Console.ReadLine().ToCharArray();
+
+        for(int i = 1; i < s.Count() / 2 + 1; i++)
+        {
+            int index1 = 2 * i - 2;
+            int index2 = 2 * i - 1;
+            char temp = s[index1];
+            s[index1] = s[index2];
+            s[index2] = temp;
+        }
+
+        Console.WriteLine(string.Join("", s));
+    }
+}
