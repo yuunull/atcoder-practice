207cde2ea547e452bc038e0681d4601d156d3040
yuunull <yuunull2525@gmail.com>
2023-11-01 23:53:20 +0900

    [add]abc294 c

---
+++ abc294/Bank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] nq = Console.ReadLine().Split();
+        int n = int.Parse(nq[0]);
+        int q = int.Parse(nq[1]);
+
+        SortedSet<int> set = new SortedSet<int>();
+        int count = 1;
+        for(int i = 0; i < q; i++)
+        {
+            string[] events = Console.ReadLine().Split();
+            switch (events[0])
+            {
+                case "1":
+                    set.Add(count);
+                    count++;
+                    break;
+                case "2":
+                    set.Remove(int.Parse(events[1]));
+                    break;
+                case "3":
+                    Console.WriteLine(set.Min);
+                    break;
+            }
+        }
+    }
+}
