6a24f9856f24275aac047eac42c516053e6ea3e4
yuunull <yuunull2525@gmail.com>
2023-11-01 22:09:38 +0900

    [add]abc294 a

---
+++ abc294/Filter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        
+        Console.WriteLine(string.Join(" ", a.Where(r => r % 2 == 0).Select(r => r).ToArray()));
+    }
+}
