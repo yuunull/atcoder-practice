587182c0479c5740107057f71b2e6529d07a80ef
yuunull <yuunull2525@gmail.com>
2023-11-02 22:31:20 +0900

    [add]abc295 a

---
+++ abc295/ProbablyEnglish/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string[] w = Console.ReadLine().Split();
+        string[] s = new string[]{"and", "not", "that", "the", "you"};
+
+        bool isYes = false;
+        for(int i = 0; i < n; i++)
+        {
+            if(s.Contains(w[i]))
+            {
+                isYes = true;
+                break;
+            }
+        }
+
+        Console.WriteLine(isYes ? "Yes" : "No");
+    }
+}
