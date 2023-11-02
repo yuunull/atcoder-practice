c063c021fd0dd0fefcc3f0cdb169a327d0313ce7
yuunull <yuunull2525@gmail.com>
2023-11-03 00:02:46 +0900

    [add]abc295 c

---
+++ abc295/Socks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int result = 0;
+
+        Array.Sort(a);
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            if(a[i] == a[i + 1])
+            {
+                result++;
+                i++;
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+}
