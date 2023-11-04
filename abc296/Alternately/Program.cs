150ef1839324b1b8a8f98025b87a2077ec26aff6
yuunull <yuunull2525@gmail.com>
2023-11-04 12:40:51 +0900

    [add]abc296 a

---
+++ abc296/Alternately/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        char[] s = Console.ReadLine().ToCharArray();
+
+        bool isYes = true;
+        for(int i = 0; i < n - 1; i++)
+        {
+            if(s[i] == s[i + 1])
+            {
+                isYes = false;
+                break;
+            }
+        }
+
+        Console.WriteLine(isYes ? "Yes" : "No");
+    }
+}
