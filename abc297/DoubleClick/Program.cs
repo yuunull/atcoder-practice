742263b0cbda3946348a4836e72fd2926cdb90da
yuunull <yuunull2525@gmail.com>
2023-11-04 15:28:31 +0900

    [add]abc297 a

---
+++ abc297/DoubleClick/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] nd = Console.ReadLine().Split();
+        int n = int.Parse(nd[0]);
+        int d = int.Parse(nd[1]);
+        int[] t = Console.ReadLine().Split().Select(int.Parse).Distinct().ToArray();
+
+        int result = -1;
+        for(int i = 0; i < n - 1; i++)
+        {
+            if(t[i + 1] - t[i] <= d)
+            {
+                result = t[i + 1];
+                break;
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
