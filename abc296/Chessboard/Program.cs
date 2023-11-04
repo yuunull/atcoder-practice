9bd372e695d51e1a5cf34db0c7ab84e33698ebe2
yuunull <yuunull2525@gmail.com>
2023-11-04 14:30:56 +0900

    [add]abc296 b

---
+++ abc296/Chessboard/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string result = "";
+        for(int i = 8; i > 0; i--)
+        {
+            char[] s = Console.ReadLine().ToCharArray();
+            for(int j = 0; j < 8; j++)
+            {
+                if(s[j] == '*')
+                {
+                    char letter = (char)('a' + j);
+                    result = letter + i.ToString();
+                }
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
