901ca2a8d8d4829ebb9ed19264da28dffefc0bc0
yuunull <yuunull2525@gmail.com>
2023-11-04 15:09:18 +0900

    [add]abc296 c

---
+++ abc296/GapExistence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] nx = Console.ReadLine().Split();
+        int n = int.Parse(nx[0]);
+        int x = int.Parse(nx[1]);
+        int[] a = Console.ReadLine().Split().Select(int.Parse).Distinct().ToArray();
+
+        List<int> list = a.Select(r => r + x).ToList();
+        var result = list.Intersect(a).ToList();
+        if (result.Count == 0)
+        {
+            Console.WriteLine("No");
+        }
+        else
+        {
+            Console.WriteLine("Yes");
+        }
+    }
+}
