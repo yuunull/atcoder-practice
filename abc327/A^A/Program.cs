78c27d45f96cef8e8e1328f48563490141b275d8
yuunull <yuunull2525@gmail.com>
2023-11-04 23:04:24 +0900

    [add]abc327 b

---
+++ abc327/A^A/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+class Program
+{
+    public static void Main()
+    {
+        long b = long.Parse(Console.ReadLine());
+
+        BigInteger result = -1;
+        BigInteger i = 1;
+        while (BigInteger.Pow(i, (int)i) <= b)
+        {
+            if (BigInteger.Pow(i, (int)i) == b)
+            {
+                result = i;
+            }
+            i++;
+        }
+        Console.WriteLine(result);
+    }
+}
