1d8617d6575d1fc75134b9a4b1cf66c90da0f117
yuunull <yuunull2525@gmail.com>
2023-11-04 17:27:09 +0900

    [add]abc297 c

---
+++ abc297/PContheTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] hw = Console.ReadLine().Split();
+        int h = int.Parse(hw[0]);
+        int w = int.Parse(hw[1]);
+
+        List<string> result = new List<string>();
+        for(int i = 0; i < h; i++)
+        {
+            string s = Console.ReadLine();
+            result.Add(s.Replace("TT","PC"));
+        }
+
+        foreach(string val in result)
+        {
+            Console.WriteLine(val);
+        }
+    }
+}
