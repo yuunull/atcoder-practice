39d4828270ca15bff612636b72d7f7797e976f7d
yuunull <yuunull2525@gmail.com>
2023-11-04 23:15:48 +0900

    [add]abc327 d

---
+++ abc327/GoodTupleProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] nm = Console.ReadLine().Split();
+        int n = int.Parse(nm[0]);
+        int m = int.Parse(nm[1]);
+        
+        string[] a = Console.ReadLine().Split();
+        string[] b = Console.ReadLine().Split();
+
+        // 解けなかった。
+
+        Console.WriteLine(isYes ? "Yes" : "No");
+    }
+}
