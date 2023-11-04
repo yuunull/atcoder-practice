ae9e2442259319ba377fff04406b2b4051464a0c
yuunull <yuunull2525@gmail.com>
2023-11-04 22:55:18 +0900

    [add]abc327 a

---
+++ abc327/ab/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int h = int.Parse(Console.ReadLine());
+        string s = Console.ReadLine();
+        Console.WriteLine(s.Contains("ab") || s.Contains("ba") ? "Yes" : "No");
+    }
+}
