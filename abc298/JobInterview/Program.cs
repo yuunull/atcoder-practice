f89562bb8a7df485c4d6ec76016af9d639a3ea23
yuunull <yuunull2525@gmail.com>
2023-11-05 10:59:21 +0900

    [add]abc298 a

---
+++ abc298/JobInterview/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        char[] s = Console.ReadLine().ToCharArray();
+
+        bool isYes = false;
+        for(int i = 0; i < n; i++)
+        {
+            if(s[i] == 'o')
+            {
+                isYes = true;
+            }
+            if(s[i] == 'x')
+            {
+                isYes = false;
+                break;
+            }
+        }
+
+        Console.WriteLine(isYes ? "Yes" : "No");
+    }
+}
