b46dbd88256f3e7f8c3bf047d2d1ae7e19c43a84
yuunull <yuunull2525@gmail.com>
2023-11-06 23:27:24 +0900

    [add]abc300 a

---
+++ abc300/NchoiceQuestion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] nab = Console.ReadLine().Split();
+        int n = int.Parse(nab[0]);
+        int a = int.Parse(nab[1]);
+        int b = int.Parse(nab[2]);
+        int[] c = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int ab =  a + b;
+        for(int i = 0; i < n; i++)
+        {
+            if(ab == c[i])
+            {
+                Console.WriteLine(i + 1);
+                break;
+            }
+        }
+    }
+}
