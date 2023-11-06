f97f4e713a8779756ce855861ab7b5f154b17dbb
yuunull <yuunull2525@gmail.com>
2023-11-06 22:47:43 +0900

    [add]abc299 a

---
+++ abc299/TreasureChest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string s = Console.ReadLine();
+
+        string s2 = s.Replace(".", "");
+        if(s2.Contains("|*|"))
+        {
+            Console.WriteLine("in");
+        }
+        else
+        {
+            Console.WriteLine("out");
+        }
+    }
+}
