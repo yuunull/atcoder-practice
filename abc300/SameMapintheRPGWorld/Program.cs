0bc58ea9488437826f10670fcfa3a5f5f8bee5d8
yuunull <yuunull2525@gmail.com>
2023-11-06 23:50:25 +0900

    [add]abc300 b

---
+++ abc300/SameMapintheRPGWorld/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] hw = Console.ReadLine().Split();
+        int h = int.Parse(hw[0]);
+        int w = int.Parse(hw[1]);
+        char[][] a = new char[h][];
+        char[][] b = new char[h][];
+
+        for(int i = 0; i < h; i++)
+        {
+            a[i] = Console.ReadLine().ToCharArray();
+        }  
+        for(int i = 0; i < h; i++)
+        {
+            b[i] = Console.ReadLine().ToCharArray();
+        }
+
+        for(int i= 0; i < h; i++)
+        {
+            for(int j= 0; j < w; j++)
+            {
+                if (CheckEqual(a, b, h, w,i,j))
+                {
+                    Console.WriteLine("Yes");
+                    return;
+                }
+            }
+        }
+
+        Console.WriteLine("No");
+    }
+
+    static bool CheckEqual(char[][] a, char[][] b, int h, int w,int x,int y)
+    {
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                if (a[(i+x)%h][(j+y)%w] != b[i][j])
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
