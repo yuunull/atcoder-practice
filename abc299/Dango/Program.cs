c47286ecc96d398cb11fb3ca7c533aaedee78191
yuunull <yuunull2525@gmail.com>
2023-11-06 23:21:13 +0900

    [add]abc299 c

---
+++ abc299/Dango/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        char[] s = Console.ReadLine().ToCharArray();
+
+        if(!s.Contains('-'))
+        {
+            Console.WriteLine(-1);
+            return;
+        }
+        
+        int count = 0;
+        int result = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(s[i] == 'o')
+            {
+                count++;
+            }
+            else
+            {
+                if(result < count)
+                {
+                    result = count;
+                }
+                count = 0;
+            }
+        }
+
+        if(count > 0 && result < count) result = count;    
+        Console.WriteLine(result == 0 ? -1 : result);
+    }
+}
