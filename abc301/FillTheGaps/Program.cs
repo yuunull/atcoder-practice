02315e0abc4b50d8045ec159a5e799735587ef68
yuunull <yuunull2525@gmail.com>
2023-11-07 21:47:20 +0900

    [add]abc301 b

---
+++ abc301/FillTheGaps/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        List<int> result = new List<int>();
+        for(int i = 0; i < n - 1; i++)
+        {
+            if(a[i] < a[i + 1])
+            {
+                int count = Math.Abs(a[i + 1] - a[i]);
+                for(int j = 0; j < count; j++)
+                {
+                    result.Add(a[i] + j);
+                }
+            }
+            else if (a[i] > a[i + 1])
+            {
+                int count = Math.Abs(a[i + 1] - a[i]);
+                for(int j = 0; j < count; j++)
+                {
+                    result.Add(a[i] - j);
+                }
+            }
+            else
+            {
+                result.Add(a[i] + 1);
+            }
+        }
+        result.Add(a[a.Count() - 1]);
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
