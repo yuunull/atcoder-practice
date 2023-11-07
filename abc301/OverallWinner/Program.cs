9206bc9c759ddd644a28e3e6d73a59441f2f47fd
yuunull <yuunull2525@gmail.com>
2023-11-07 21:32:31 +0900

    [add]abc301 a

---
+++ abc301/OverallWinner/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string s = Console.ReadLine();
+        int t = s.Count(r => r == 'T');
+        int a = s.Count(r => r == 'A');
+
+        if(t > a)
+        {
+            Console.WriteLine('T');
+            return;
+        }
+
+        if(t < a)
+        {
+            Console.WriteLine('A');
+            return;
+        }
+
+        if(t == a)
+        {
+            int tTesult = t;
+            int aTesult = a;
+            for(int i = 0; i < n; i++)
+            {
+                if(s[i] == 'T')
+                {
+                    tTesult--;
+                    if(tTesult == 0)
+                    {
+                        Console.WriteLine('T');
+                        break;
+                    }
+                }
+                if(s[i] == 'A')
+                {
+                    aTesult--;
+                    if(aTesult == 0)
+                    {
+                        Console.WriteLine('A');
+                        break;
+                    }
+                }
+                
+            }
+        }
+
+        
+    }
+}
