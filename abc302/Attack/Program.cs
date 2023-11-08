73353b3fe429c08881912b69481cf36839f2eb7d
yuunull <yuunull2525@gmail.com>
2023-11-08 22:16:00 +0900

    [add]abc302 a

---
+++ abc302/Attack/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] ab = Console.ReadLine().Split();
+        long a = long.Parse(ab[0]);
+        long b = long.Parse(ab[1]);
+
+        long result = a / b + (a % b == 0 ? 0 : 1);
+        Console.WriteLine(result);
+    }
+}
