5c675992b638846f82ffc3e70a7b9b3845e936c5
yuunull <yuunull2525@gmail.com>
2023-11-11 23:15:42 +0900

    [add]abc328 a

---
+++ abc328/NotTooHard/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] nx = Console.ReadLine().Split();
+        int n = int.Parse(nx[0]);
+        int x = int.Parse(nx[1]);
+        int[] s = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int result = s.Where(r => r <= x).Sum();
+        Console.WriteLine(result);
+    }
+}
