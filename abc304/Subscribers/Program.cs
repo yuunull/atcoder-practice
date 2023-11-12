b184b7f1be2ec03ea439434741412f37499732d9
yuunull <yuunull2525@gmail.com>
2023-11-12 21:55:27 +0900

    [add]abc304 b

---
+++ abc304/Subscribers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int k = (int)Math.Floor(Math.Log10(n));
+        if(k < 3)
+        {
+            Console.WriteLine(n);
+        }
+        else
+        {
+            Console.WriteLine(n -( n % (int)Math.Pow(10, k - 2)));
+        }
+    }
+}
