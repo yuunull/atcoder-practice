7d36045bac6f55c3c2f38af1380a21aef35f8c9e
yuunull <yuunull2525@gmail.com>
2023-11-16 21:14:46 +0900

    [add]90 033

---
+++ Typical90Questions/033_NotTooBright/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] hw = Console.ReadLine().Split();
+        int h = int.Parse(hw[0]);
+        int w = int.Parse(hw[1]);
+
+        if(h == 1)
+        {
+            Console.WriteLine(w);
+        }
+        else if(w == 1)
+        {
+            Console.WriteLine(h);
+        }
+        else
+        {
+            var H = (double)h / 2;
+            var h1 = Math.Ceiling(H);
+            var W = (double) w / 2;
+            var w1 = Math.Ceiling(W);
+
+            var result = (int) h1 * w1;
+
+            Console.WriteLine(result);
+        }
+    }
+}
