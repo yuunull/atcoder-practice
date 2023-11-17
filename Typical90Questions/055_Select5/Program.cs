381846efc8e695a021cf77c6b61bfbefc6d92bce
yuunull <yuunull2525@gmail.com>
2023-11-17 23:47:29 +0900

    [add]90 055

---
+++ Typical90Questions/055_Select5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] npq = Console.ReadLine().Split();
+        int n = int.Parse(npq[0]);
+        int p = int.Parse(npq[1]);
+        int q = int.Parse(npq[2]);
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int result = 0;
+        for(int i = 0; i < n - 5; i++)
+        {
+            long sum = 1;
+            for(int j = i; j < i + 5; j++)
+            {
+                sum *= a[j];
+            }
+            if(sum % p == q)
+            {
+                result++;
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
