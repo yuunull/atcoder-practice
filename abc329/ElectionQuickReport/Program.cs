deb57dbe0bc5f0ea39b7acee9cf39f1737a42ab0
yuunull <yuunull2525@gmail.com>
2023-11-18 22:49:29 +0900

    [add]abc329 d

---
+++ abc329/ElectionQuickReport/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] nm = Console.ReadLine().Split();
+        int n = int.Parse(nm[0]);
+        int m = int.Parse(nm[1]);
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int[] array = new int[n + 1];
+        int index = -1;
+        int max = 0;
+        for(int i = 0; i < m; i++)
+        {
+            array[a[i]] += 1;
+            if(max < array[a[i]])
+            {
+                max = array[a[i]];
+                index = a[i];
+            }
+            if(max == array[a[i]] && index > a[i])
+            {
+                index = a[i];
+            }
+            Console.WriteLine(index);
+        }
+   }
+}
