4475079c47872c507c1d499b8c643843e94fd554
yuunull <yuunull2525@gmail.com>
2023-11-18 22:49:02 +0900

    [add]abc329 b

---
+++ abc329/Next/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int max = a.Max();
+        int result = a.Where(r => r != max).Max();
+        Console.WriteLine(result);
+   }
+}
