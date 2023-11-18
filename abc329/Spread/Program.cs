3acd877cbf05d87328c2bc2b487b0bd6d150de6f
yuunull <yuunull2525@gmail.com>
2023-11-18 22:48:51 +0900

    [add]abc329 a

---
+++ abc329/Spread/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        char[] s = Console.ReadLine().ToCharArray();
+        Console.WriteLine(string.Join(" ", s));
+   }
+}
