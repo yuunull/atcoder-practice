4e5f0f6124eaf80d186059e0e82bfb6e27344b8f
yuunull <yuunull2525@gmail.com>
2023-11-22 22:18:26 +0900

    [add]90 067

---
+++ Typical90Questions/067_Base8to9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    /// <summary>
+    ///  この問題は苦手
+    ///  あとでじっくりやる
+    /// </summary>
+    public static void Main()
+    {
+
+    }
+}
