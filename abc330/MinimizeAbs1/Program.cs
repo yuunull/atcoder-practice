c9cb98bba5c29f13b6c9c4a0705038540a5c632f
yuunull <yuunull2525@gmail.com>
2023-11-25 23:05:36 +0900

    [add]abc330 b

---
+++ abc330/MinimizeAbs1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] nlr = Console.ReadLine().Split();
+        int n = int.Parse(nlr[0]);
+        long l = int.Parse(nlr[1]);
+        long r = int.Parse(nlr[2]);
+        long[] a = Console.ReadLine().Split().Select(long.Parse).ToArray();
+        
+        List<long> array = new List<long>();
+        for(int i = 0; i < n; i++)
+        {
+            if(a[i] >= l && a[i] < r)
+            {
+                array.Add(a[i]);
+                continue;
+            }
+
+            if(a[i] < l)
+            {
+                array.Add(l);
+                continue;
+            }
+
+            if(a[i] >= r)
+            {
+                array.Add(r);
+                continue;
+            }
+        }
+        Console.WriteLine(string.Join(" ", array));
+    }
+}
