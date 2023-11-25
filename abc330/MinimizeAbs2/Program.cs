4381cfdf175bb56fdda64df874b12c50570b2167
yuunull <yuunull2525@gmail.com>
2023-11-25 23:05:46 +0900

    [add]abc330 c

---
+++ abc330/MinimizeAbs2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Math;
+
+class Program
+{
+    public static void Main()
+    {
+        long D = long.Parse(Console.ReadLine());
+
+        long min = long.MaxValue;
+
+        for (long x = 1; x * x <= D; x++) 
+        {
+            long y = (long)Sqrt(D - x * x);
+
+            min = Min(min, Abs(x * x + y * y - D));
+            if (y >= 2) 
+            {
+                min = Min(min, Abs(x * x + (y - 1) * (y - 1) - D));
+            };
+            min = Min(min, Abs(x * x + (y + 1) * (y + 1) - D));
+        }
+
+        Console.WriteLine(min);
+    }
+}
