2357e772dfb946d52cd1e2efefede76cbc16ca9c
yuunull <yuunull2525@gmail.com>
2023-11-25 23:05:26 +0900

    [add]abc330 a

---
+++ abc330/CountingPasses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] nl = Console.ReadLine().Split();
+        int n = int.Parse(nl[0]);
+        int l = int.Parse(nl[1]);
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        
+        int result = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(a[i] >= l)
+            {
+                result++;
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
