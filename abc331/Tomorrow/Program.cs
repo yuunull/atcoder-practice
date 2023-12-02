ebb10f8373571228d19c87982849a13edeb3e250
yuunull <yuunull2525@gmail.com>
2023-12-02 22:48:57 +0900

    [add]abc331 a

---
+++ abc331/Tomorrow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Math;
+
+class Program
+{
+    public static void Main()
+    {
+        string[] md = Console.ReadLine().Split();
+        int m = int.Parse(md[0]);
+        int d = int.Parse(md[1]);
+        int[] ymd = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        if(ymd[2] + 1 > d)
+        {
+            if(ymd[1] + 1 > m)
+            {
+                Console.WriteLine((ymd[0] + 1) + " " + 1 + " " + 1);
+            }
+            else
+            {
+                Console.WriteLine(ymd[0] + " " + (ymd[1] + 1) + " " + 1);
+            }
+        }
+        else
+        {
+            Console.WriteLine(ymd[0] + " " + ymd[1] + " " + (ymd[2] + 1));
+        }
+        
+    }
+}
