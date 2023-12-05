d58d96b8d28be73d9e33bcb61b575d63f7149e0e
yuunull <yuunull2525@gmail.com>
2023-12-05 23:44:04 +0900

    [update]90 016

---
+++ Typical90Questions/016_MinimumCoins/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+class Program
+{
+	static void Main()
+	{
+        int n = int.Parse(Console.ReadLine());
+        int[] abc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        long result = 9999;
+
+        for (long i = 0; i <= 9999; i++)
+        {
+            for (long j = 0; j < 9999 - i; j++)
+            {
+                long zan = n - (abc[0] * i + abc[1] * j);
+                if (zan < 0) continue;
+                if (zan % abc[2] == 0)
+                {
+                    result = Math.Min(result, i + j + (zan / abc[2]));
+                }
+            }
+        }
+
+        Console.WriteLine(result);
+	}
+}
