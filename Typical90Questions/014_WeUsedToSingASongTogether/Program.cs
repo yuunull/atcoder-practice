2bbd618011da311d5d8b0a638d4a8a9585385fcb
yuunull <yuunull2525@gmail.com>
2023-12-05 23:07:55 +0900

    [add]90 014

---
+++ Typical90Questions/014_WeUsedToSingASongTogether/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+class Program
+{
+    /// <summary>
+    /// ・aとbの配列をソートする。
+    /// ・aとbの配列を最初の要素から引き算しその絶対値を合計していく
+    /// </summary>
+	static void Main()
+	{
+        int n = int.Parse(Console.ReadLine());
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] b = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        List<int> sortedA = a.OrderBy(r => r).ToList();
+        List<int> sortedB = b.OrderBy(r => r).ToList();
+
+        long result = 0;
+        for(int i = 0; i < n; i++)
+        {
+            result += Math.Abs(sortedA[i] - sortedB[i]);
+        }
+        Console.WriteLine(result);
+	}
+}
