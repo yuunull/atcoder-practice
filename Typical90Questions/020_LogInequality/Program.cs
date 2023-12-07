02fc5aa61dd24031867187521d0c294d810483b3
yuunull <yuunull2525@gmail.com>
2023-12-07 23:40:07 +0900

    [add]90 020

---
+++ Typical90Questions/020_LogInequality/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+class Program
+{
+	static void Main()
+	{
+        int[] abc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        double log2a = Math.Log(abc[0], 2);
+        double bLogsc = Math.Log(abc[2], 2) * abc[1];
+        Console.WriteLine(log2a < bLogsc ? "Yes" : "No");
+	}
+}
